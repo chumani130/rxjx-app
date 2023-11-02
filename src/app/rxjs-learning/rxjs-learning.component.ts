a8dfd50c34953727ee47bc3e0f66686409b4a065
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-02 11:50:45 +0200

    Create rxjs-learning.component.ts

---
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-learning',
+  templateUrl: './rxjs-learning.component.html',
+  styleUrls: ['./rxjs-learning.component.scss']
+})
+export class RxjsLearningComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
