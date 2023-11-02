dd42eec90acd76294f553ae2d14fa5b596553950
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-02 11:51:05 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RxjsLearningComponent } from './rxjs-learning/rxjs-learning.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'rxjs-learning', component: RxjsLearningComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
