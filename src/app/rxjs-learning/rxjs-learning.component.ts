f88268cd94102c0263639234acb27f640e114720
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-02 14:47:41 +0200

    Update rxjs-learning.component.ts

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -1,32 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of, interval } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-learning',
   templateUrl: './rxjs-learning.component.html',
   styleUrls: ['./rxjs-learning.component.scss']
 })
-export class RxjsLearningComponent  implements OnInit {
+export class RxjsLearningComponent implements OnInit {
+  agents!: Observable<string[]>;
+  agentName: string = ''; // Initialize agentName as an empty string
 
-  agents!: Observable<string>;
   constructor() { }
 
   ngOnInit(): void {
-    this.agents = new Observable(
-      function(observer){
-        try {
-          observer.next('Ram');
-          observer.next('Mark');
-          observer.next('Sita');
-        }
-        catch(e) {
-          observer.error(e);
-        }
+    this.agents = new Observable<string[]>((observer) => {
+      try {
+        observer.next(['Ram']);
+        interval(3000).subscribe(() => {
+          observer.next(['Mark']);
+        });
+        interval(6000).subscribe(() => {
+          observer.next(['Sita']);
+        });
+        // Don't complete the observable here.
+      } catch (e) {
+        observer.error(e);
       }
-    );
-    this.agents.subscribe(data => {
-      console.log(data);
-    })
-  }
+    });
 
+    // You can't directly assign an observable to agentName. Instead, extract values from the observable.
+    this.agents.subscribe((data: string[]) => {
+      this.agentName = data[0]; // Assign the first agent name to agentName.
+    });
+  }
 }
