17236472f6d6a31342d554f050dfc80cbcc16a00
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2023-11-03 09:13:28 +0200

    Update rxjs-learning.component.ts

--- src/app/rxjs-learning/rxjs-learning.component.ts
+++ src/app/rxjs-learning/rxjs-learning.component.ts
@@ -7,30 +7,62 @@ import { Observable, of, interval } from 'rxjs';
   styleUrls: ['./rxjs-learning.component.scss']
 })
 export class RxjsLearningComponent implements OnInit {
-  agents!: Observable<string[]>;
-  agentName: string = ''; // Initialize agentName as an empty string
+  agents!: Observable<string>
+  ageName!: string;
+
+  studentList = ['mark', 'Ram', 'Sita', 'Lisa'];
+  students: Observable<string[]> = of(this.studentList);
+
+  studentNames: Observable<string> = of('Ram');
+
+  studentObj = {id: 10, name: 'Ram'}
+  student$: Observable<any> = of(this.studentObj);
 
   constructor() { }
 
   ngOnInit(): void {
-    this.agents = new Observable<string[]>((observer) => {
-      try {
-        observer.next(['Ram']);
-        interval(3000).subscribe(() => {
-          observer.next(['Mark']);
-        });
-        interval(6000).subscribe(() => {
-          observer.next(['Sita']);
-        });
-        // Don't complete the observable here.
-      } catch (e) {
-        observer.error(e);
-      }
-    });
 
-    // You can't directly assign an observable to agentName. Instead, extract values from the observable.
-    this.agents.subscribe((data: string[]) => {
-      this.agentName = data[0]; // Assign the first agent name to agentName.
-    });
+    this.students.subscribe(data => {
+      console.log(data);
+    })
+    this.studentNames.subscribe(data => {
+      console.log(data);
+    })
+    this.student$.subscribe(data => {
+      console.log(data);
+    })
+
   }
-}
+ }
+
+// for creating observables
+
+
+//export class RxjsLearningComponent implements OnInit {
+//   agents!: Observable<string[]>;
+//   agentName: string = ''; // Initialize agentName as an empty string
+
+//   constructor() { }
+
+//   ngOnInit(): void {
+//     this.agents = new Observable<string[]>((observer) => {
+//       try {
+//         observer.next(['Ram']);
+//         interval(3000).subscribe(() => {
+//           observer.next(['Mark']);
+//         });
+//         interval(6000).subscribe(() => {
+//           observer.next(['Sita']);
+//         });
+//         // Don't complete the observable here.
+//       } catch (e) {
+//         observer.error(e);
+//       }
+//     });
+
+//     // You can't directly assign an observable to agentName. Instead, extract values from the observable.
+//     this.agents.subscribe((data: string[]) => {
+//       this.agentName = data[0]; // Assign the first agent name to agentName.
+//     });
+//   }
+// }
